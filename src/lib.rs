74addf3d599a70af5061befd8b3e19d185aaf492
Léo Gaspard <leo@gaspard.io>
2018-04-15 22:38:55 +0200

    Improve API

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,35 @@
 extern crate tokio;
 
+use std::io::BufRead;
 use tokio::prelude::*;
+
+pub type MailAddress = Vec<u8>;
+pub type MailAddressRef<'a> = &'a [u8];
+
+pub struct ConnectionMetadata {}
+
+pub struct MailMetadata {
+    from: MailAddress,
+    to: Vec<MailAddress>,
+}
+
+pub struct Refusal {
+    code: u16,
+    msg: String,
+}
 
 pub enum Decision {
     Accept,
-    Reject,
+    Reject(Refusal),
 }
-
-pub struct Metadata {
-    from: String,
-    to: Vec<String>,
-}
-
-pub enum Error {}
 
 pub fn interact<
-    R: AsyncRead,
-    W: AsyncWrite,
-    F: FnMut(Metadata, &Stream<Item = String, Error = Error>) -> Decision,
->(incoming: R, outgoing: W, handler: F) {
+    Reader: AsyncRead + BufRead,
+    Writer: AsyncWrite,
+    State,
+    FilterFrom: FnMut(MailAddressRef, &ConnectionMetadata) -> Result<State, Refusal>,
+    FilterTo: FnMut(MailAddressRef, State, &ConnectionMetadata, &MailMetadata)
+                    -> Result<State, Refusal>,
+    HandleMail: FnMut(MailMetadata, State, &AsyncRead) -> Decision,
+>(incoming: Reader, outgoing: Writer, filter_from: FilterFrom, filter_to: FilterTo, handler: HandleMail){
 }
