445f2c4b686cc25999f0e6335d192073fd245a44
Léo Gaspard <leo@gaspard.io>
2018-06-06 10:10:59 +0200

    smtp-queue: parameterize mails depending on metadata

--- smtp-queue/src/mail.rs
+++ smtp-queue/src/mail.rs
@@ -9,6 +9,9 @@ pub struct Mail<S: Stream<Item = BytesMut, Error = ()>, M> {
     pub metadata: M,
 }
 
-pub trait QueuedMail {}
+pub trait QueuedMail<M> {}
 
-pub trait InflightMail {}
+// TODO: (B) replace all these Box by impl Trait syntax hide:impl-trait-in-trait
+pub trait InflightMail<M> {
+    fn get_mail(&self) -> Mail<Box<Stream<Item = BytesMut, Error = ()>>, M>;
+}
--- smtp-queue/src/storage.rs
+++ smtp-queue/src/storage.rs
@@ -6,7 +6,7 @@ use mail::{InflightMail, Mail, QueuedMail};
 // TODO: (B) replace all these Box by impl Trait syntax hide:impl-trait-in-trait
 // TODO: (B) for a clean api, the futures should not take ownership and return
 // but rather take a reference (when async/await will be done)
-pub trait Storage<M, QM: QueuedMail, IM: InflightMail>: Sized {
+pub trait Storage<M, QM: QueuedMail<M>, IM: InflightMail<M>>: Sized {
     fn list_queue(&self) -> Box<Stream<Item = QM, Error = ()>>;
     fn list_inflight(&self) -> Box<Stream<Item = IM, Error = ()>>;
 
