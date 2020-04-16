bb2478e56c02f6a8a1679e8578505c31d80b19b4
Léo Gaspard <leo@gaspard.io>
2020-04-17 00:55:18 +0200

    smtp-message: add benches for data stream and sink

--- smtp-message/benches/benches.rs
+++ smtp-message/benches/benches.rs
@@ -2,8 +2,9 @@
 
 extern crate test;
 
-use bytes::Bytes;
-use smtp_message::Command;
+use bytes::{Bytes, BytesMut};
+use futures::{executor, future, stream};
+use smtp_message::{Command, DataSink, DataStream, StreamExt};
 use test::Bencher;
 
 #[bench]
@@ -31,4 +32,27 @@ fn parse_command(b: &mut Bencher) {
         }
     });
     b.bytes = commands.iter().map(|b| b.len() as u64).sum();
+}
+
+#[bench]
+fn parse_data_stream(b: &mut Bencher) {
+    let mut data = BytesMut::new();
+    data.extend(&b"Blah blah blah...\r\n...etc. etc. etc.\r\n.\r\n"[..]);
+    b.iter(|| {
+        let mut stream = stream::once(future::ready(data.clone())).prependable();
+        test::black_box(executor::block_on_stream(&mut DataStream::new(&mut stream)).count());
+    });
+    b.bytes = data.len() as u64;
+}
+
+#[bench]
+fn output_data_sink(b: &mut Bencher) {
+    let data = Bytes::from_static(b"Blah blah blah...\r\n..etc. etc. etc.\r\n");
+    let mut v = Vec::with_capacity(1024);
+    b.iter(|| {
+        let mut sink = DataSink::new(&mut v);
+        test::black_box(executor::block_on(sink.send(data.clone())).unwrap());
+        test::black_box(executor::block_on(sink.end()).unwrap());
+    });
+    b.bytes = data.len() as u64;
 }
