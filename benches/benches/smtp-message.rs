e7f2e2f118da40f33f393edd3799a617623cf52f
Léo Gaspard <leo@gaspard.io>
2020-05-15 17:55:27 +0200

    benches: reduce branch predictor learnability

--- benches/benches/smtp-message.rs
+++ benches/benches/smtp-message.rs
@@ -1,33 +1,83 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+#![allow(unused_must_use)]
+
+use criterion::{
+    black_box, criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion,
+    PlotConfiguration, Throughput,
+};
 
 use smtp_message::Email;
 
 pub fn parse_email(c: &mut Criterion) {
-    let tests: &[&[u8]] = &[
-        b"postmaster",
-        b"t+e-s.t_i+n-g@foo.bar.baz",
-        br#""quoted\"example"@example.org"#,
+    let tests: &[&[&[u8]]] = &[
+        &[b"postmaster", b"test", b"foobar", b"root"],
+        &[
+            b"t+e-s.t_i+n-g@foo.bar.baz",
+            b"hello@world.com",
+            b"this.is.a.very.long.message@lists.subdomain.domain.tld",
+        ],
+        &[
+            br#""quoted\"example"@example.org"#,
+            br#""and with\@stuff like this\"\\"@test.com"#,
+        ],
     ];
     let names: &[&str] = &["localpart only", "normal email", "quoted-string localpart"];
+
     let mut g = c.benchmark_group("Email::parse");
+    // https://github.com/38/plotters/issues/143
+    // g.plot_config(PlotConfiguration::default().summary_scale(AxisScale::
+    // Logarithmic));
+
     for i in 0..tests.len() {
         let n = names[i];
-        g.bench_with_input(BenchmarkId::new("smtp-message", n), &i, |b, i| {
-            b.iter(|| Email::<&str>::parse(tests[*i]))
-        });
-        g.bench_with_input(BenchmarkId::new("smtp-message-alloc", n), &i, |b, i| {
-            b.iter(|| Email::<String>::parse(tests[*i]))
-        });
-        g.bench_with_input(BenchmarkId::new("rustyknife-legacy", n), &i, |b, i| {
+        g.throughput(Throughput::Bytes(
+            tests[i].iter().map(|s| s.len() as u64).sum(),
+        ));
+        // https://github.com/bheisler/criterion.rs/issues/382
+        // g.throughput(Throughput::Elements(tests[i].len() as u64));
+        g.bench_with_input(BenchmarkId::new("smtp-message", n), tests[i], |b, tests| {
             b.iter(|| {
-                rustyknife::rfc5321::validate_address::<rustyknife::behaviour::Legacy>(tests[*i])
+                for t in tests {
+                    black_box(Email::<&str>::parse(t));
+                }
             })
         });
-        g.bench_with_input(BenchmarkId::new("rustyknife-intl", n), &i, |b, i| {
-            b.iter(|| {
-                rustyknife::rfc5321::validate_address::<rustyknife::behaviour::Intl>(tests[*i])
-            })
-        });
+        g.bench_with_input(
+            BenchmarkId::new("smtp-message-alloc", n),
+            tests[i],
+            |b, tests| {
+                b.iter(|| {
+                    for t in tests {
+                        black_box(Email::<String>::parse(t));
+                    }
+                })
+            },
+        );
+        g.bench_with_input(
+            BenchmarkId::new("rustyknife-legacy", n),
+            tests[i],
+            |b, tests| {
+                b.iter(|| {
+                    for t in tests {
+                        black_box(rustyknife::rfc5321::validate_address::<
+                            rustyknife::behaviour::Legacy,
+                        >(t));
+                    }
+                })
+            },
+        );
+        g.bench_with_input(
+            BenchmarkId::new("rustyknife-intl", n),
+            tests[i],
+            |b, tests| {
+                b.iter(|| {
+                    for t in tests {
+                        black_box(rustyknife::rfc5321::validate_address::<
+                            rustyknife::behaviour::Intl,
+                        >(t));
+                    }
+                })
+            },
+        );
     }
 }
 
