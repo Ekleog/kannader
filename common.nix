5ee49aa790c01778211b2f0ff3fa8e3e95c65bd2
Léo Gaspard <leo@gaspard.io>
2020-12-14 18:14:42 +0100

    nix: stop relying on host nixpkgs

--- common.nix
+++ common.nix
@@ -1,17 +1,12 @@
 rec {
-  hostPkgs = import <nixpkgs> {};
-  pkgsSrc = hostPkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
+  pkgsSrc = builtins.fetchTarball {
     # The following is for nixos-unstable on 2020-12-13
-    rev = "e9158eca70ae59e73fae23be5d13d3fa0cfc78b4";
+    url = "https://github.com/NixOS/nixpkgs/archive/e9158eca70ae59e73fae23be5d13d3fa0cfc78b4.tar.gz";
     sha256 = "0cnmvnvin9ixzl98fmlm3g17l6w95gifqfb3rfxs55c0wj2ddy53";
   };
-  rustOverlaySrc = hostPkgs.fetchFromGitHub {
-    owner = "mozilla";
-    repo = "nixpkgs-mozilla";
+  rustOverlaySrc = builtins.fetchTarball {
     # The following is the latest version as of 2020-12-13
-    rev = "8c007b60731c07dd7a052cce508de3bb1ae849b4";
+    url = "https://github.com/mozilla/nixpkgs-mozilla/archive/8c007b60731c07dd7a052cce508de3bb1ae849b4.tar.gz";
     sha256 = "1zybp62zz0h077zm2zmqs2wcg3whg6jqaah9hcl1gv4x8af4zhs6";
   };
   rustOverlay = import rustOverlaySrc;
