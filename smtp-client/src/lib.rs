a0c6349952e4ba1cd1674fd01ddb7b02af76013f
Léo Gaspard <leo@gaspard.io>
2020-12-22 01:50:24 +0100

    smtp-client: implement MX resolution

--- smtp-client/src/lib.rs
+++ smtp-client/src/lib.rs
@@ -1,8 +1,9 @@
-use std::{io, net::IpAddr, pin::Pin};
+use std::{collections::BTreeMap, io, net::IpAddr, pin::Pin};
 
 use futures::{AsyncRead, AsyncReadExt, AsyncWrite};
+use rand::prelude::SliceRandom;
 use smol::net::TcpStream;
-use trust_dns_resolver::AsyncResolver;
+use trust_dns_resolver::{AsyncResolver, IntoName};
 
 use smtp_message::{Email, Hostname};
 
@@ -37,19 +38,66 @@ where
 
     pub async fn connect(&self, dest: &Destination) -> io::Result<Sender> {
         match dest.host {
-            Hostname::Ipv4 { ip, .. } => self.connect_ip(IpAddr::V4(ip)).await,
-            Hostname::Ipv6 { ip, .. } => self.connect_ip(IpAddr::V6(ip)).await,
-            Hostname::AsciiDomain { ref raw } => self.connect_host(&raw).await,
-            Hostname::Utf8Domain { ref punycode, .. } => self.connect_host(&punycode).await,
+            Hostname::Ipv4 { ip, .. } => self.connect_to_ip(IpAddr::V4(ip)).await,
+            Hostname::Ipv6 { ip, .. } => self.connect_to_ip(IpAddr::V6(ip)).await,
+            Hostname::AsciiDomain { ref raw } => self.connect_to_mx(&raw).await,
+            Hostname::Utf8Domain { ref punycode, .. } => self.connect_to_mx(&punycode).await,
         }
     }
 
-    async fn connect_host(&self, host: &str) -> io::Result<Sender> {
-        let _ = (host, &self.resolver);
+    pub async fn connect_to_mx(&self, host: &str) -> io::Result<Sender> {
+        // TODO: consider adding a `.` at the end of `host`... but is it
+        // actually allowed?
+        // Run MX lookup
+        let lookup = self.resolver.mx_lookup(host).await?;
+
+        // Retrieve the actual records
+        let mut mx_records = BTreeMap::new();
+        for record in lookup.iter() {
+            mx_records
+                .entry(record.preference())
+                .or_insert(Vec::with_capacity(1))
+                .push(record.exchange());
+        }
+
+        // If there are no MX records, try A/AAAA records
+        if mx_records.is_empty() {
+            return self.connect_to_host(&host.into_name()?).await;
+        }
+
+        // By increasing order of priority, try each MX
+        let mut first_error = None;
+        for (_, mut mxes) in mx_records {
+            // Among a single priority level, randomize the order
+            // TODO: consider giving a way to seed for reproducibility?
+            mxes.shuffle(&mut rand::thread_rng());
+
+            // Then try to connect to each address
+            for mx in mxes {
+                match self.connect_to_host(mx).await {
+                    Ok(sender) => return Ok(sender),
+                    Err(e) => first_error = first_error.or(Some(e)),
+                }
+            }
+        }
+
+        // The below unwrap is safe because, to reach it:
+        // - there must be some MX records or we'd have returned in the if above
+        // - there have been no error as otherwise first_error wouldn't be None
+        // - there must have only be errors as otherwise we'd have returned in the match
+        //   above
+        // Hence, if it triggers it means that \exists N, N > 1 \wedge N = 0, where N is
+        // the number of errors.
+        //   QED.
+        Err(first_error.unwrap())
+    }
+
+    async fn connect_to_host(&self, name: &trust_dns_resolver::Name) -> io::Result<Sender> {
+        let _ = name;
         todo!()
     }
 
-    async fn connect_ip(&self, ip: IpAddr) -> io::Result<Sender> {
+    pub async fn connect_to_ip(&self, ip: IpAddr) -> io::Result<Sender> {
         let io = TcpStream::connect((ip, SMTP_PORT)).await?;
         let (reader, writer) = io.split();
         Ok(Sender {
@@ -83,3 +131,5 @@ impl Sender {
         todo!()
     }
 }
+
+// TODO: add tests
