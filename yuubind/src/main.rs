9b62a0ca4b56043f4ac71068f0e28201aaad3bec
Léo Gaspard <leo@gaspard.io>
2020-12-23 21:25:47 +0100

    yuubind: init crate

---
+++ yuubind/src/main.rs
@@ -0,0 +1,7 @@
+// TODO: split into multiple processes, with multiple uids (stretch goal: do not
+// require root and allow the user to directly call multiple executables and
+// pass it the pre-opened sockets)
+
+fn main() {
+    todo!()
+}
